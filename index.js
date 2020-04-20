0a7c22542e8a306d82a0d0cf2d61f16235126edd
Bennett Cooper <bwc@clemson.edu>
2020-04-20 11:20:51 -0400

    navigation skeleton and about page

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-function transitionCollapsible(collapsible) {
+function transitionCollapsible(collapsible){
   console.log(collapsible.checked);
 
   var content = collapsible.parentElement.nextElementSibling;
 
   if(collapsible.checked == true){
     content.style.maxHeight = "100vh";
-  } else {
+  }else{
     content.style.maxHeight = "0";
   }
 
