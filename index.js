6eecc7da55ba5d5171cffb5fb9460e77b4b8ba5b
Bennett Cooper <bwc@clemson.edu>
2020-04-27 10:18:19 -0400

    formatting, people page, more stuff done through js, merge about and home

--- index.js
+++ index.js
@@ -10,3 +10,31 @@ function transitionCollapsible(collapsible){
   }
 
 }
+
+function createTopNav(){
+  var a = document.createElement("DIV")
+  a.innerHTML = "<a href=\"./index.html\">Home</a>\
+  <a href=\"./Documents\">Documents</a>\
+  <a href=\"./results.html\">Results</a>\
+  <a href=\"./people.html\">People</a>";
+  a.setAttribute("class", "top_navbar");
+  document.body.appendChild(a);
+}
+/*
+function createPerson(name, ){
+  var a = document.createElement("P");
+  a.innerHTML = `<img align=\"left\" alt=\"${name}\" src=\"./images/people/${name}.png\">
+  <b>${name}</b>
+  <br>
+  <u>Involvement</u>
+  <br>
+  Capstone Spring 2020
+  <br>
+  Creative Inquiry 19-20`;
+  a.setAttribute("class", "person")
+  document.getElementById('people').appendChild(a);
+}
+*/
+window.onload = function(){
+  createTopNav();
+};
